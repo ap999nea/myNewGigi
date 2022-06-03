e7f4a54718e71b5af4242d83693a0b59832d82e7
Stefan <houseanonv1@gmail.com>
2022-06-03 23:15:53 +0200

    cacca paolo gino bagno

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { httpGetDashboards } from "./api.http";
 import Bucket from "./Bucket";
 import Card from "./Card";
 import { Form } from "./Form";
@@ -9,15 +10,50 @@ class Dashboard extends React.Component {
   // }
   state = { cardName: "Inizio" };
   componentDidMount() {
-    this.setState({ cardName: "La mia carta" });
-    console.log("Sono montato1");
+    this.setState({
+      cardName: "La mia carta",
+    });
+    this.getMyDashboards();
   }
+  async getMyDashboards() {
+    let myDashboard = {};
+    try {
+      const resp = await httpGetDashboards();
+      console.log(resp.data);
+      myDashboard = resp.data;
+    } catch (err) {
+      console.log(err);
+    }
+    this.setState((prev) => {
+      return {
+        ...prev,
+        myDashboard: myDashboard,
+      };
+    });
+  }
+
   componentDidUpdate() {
     console.log("Sono aggiornato");
   }
   render() {
     console.log("Render");
-    return <div style={{ padding: 50 }} className="dashboard"></div>;
+    const { myDashboard } = this.state;
+    console.log(myDashboard);
+    let appoggio;
+    if (myDashboard) {
+      appoggio = myDashboard.map((el) => {
+        return <Bucket title={el.name}></Bucket>;
+      });
+    } else {
+      appoggio = <h1>Cacca</h1>;
+    }
+    return (
+      <>
+        <div style={{ padding: 50 }} className="dashboard">
+          {appoggio}
+        </div>
+      </>
+    );
   }
 }
 export default Dashboard;
--- src/api.http.js
+++ src/api.http.js
@@ -1,7 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
-export const httpCreateCard = (data)=>{
-    const url = "https://run.mocky.io/v3/bf2bf8df-6cbc-4042-a3c4-897333af2111"
-    return axios.post(url,{data})
-}
+export const httpCreateCard = (data) => {
+  const url = "https://run.mocky.io/v3/bf2bf8df-6cbc-4042-a3c4-897333af2111";
+  return axios.post(url, { data });
+};
 
+export const httpGetDashboards = () => {
+  const url = "http://localhost:1000/app/dashboards";
+  return axios.get(url);
+};
